fa6ab3b342d685a23a4c4e8db6078534730dfe6b
lfbesa <lfbesa@uc.cl>
2018-03-15 17:59:19 -0300

    aaa

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :news
-  belongs_to :user
-  validates :body, presence: true, allow_blank: false
-end
--- app/views/comments/_comment.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! comment, :id, :news_id, :usuario, :comentario, :created_at, :updated_at
-json.url comment_url(comment, format: :json)
