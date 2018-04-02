cd97aea938677377088348ab6a6efc1d52d85ef4
lfbesa <lfbesa@uc.cl>
2018-04-02 13:10:21 -0300

    arreglar header

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
   before_action :set_news, only: [:show, :edit, :update, :destroy]
-
+  ActionDispatch::Response::default_charset = nil
   # GET /news
   # GET /news.json
   def index
