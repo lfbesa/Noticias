c9913edee72dd707c2b08519401a03ee4b67907b
lfbesa <lfbesa@uc.cl>
2018-04-02 12:35:59 -0300

    arreglar header

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,6 +3,7 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
+    response.headers['Content-Type'] = 'application/json'
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
